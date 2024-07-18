0c210669a5dae95808e687a7065ae899bddedb19
rodrigo-rrp <rodrigovanoni@gmail.com>
2024-07-18 14:35:46 -0300

    finish activity feed

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,61 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Box,
+  Button,
+  Chip,
   List,
   ListItem,
   ListItemButton,
-  ListItemIcon,
   ListItemSecondaryAction,
   ListItemText,
 } from '@mui/material'
 import { useActivityContext } from '../contexts/activityContext'
 
 import LoadingOverlay from './LoadingOverlay.jsx'
-import { CallMade, CallReceived, Phone, PhoneMissed } from '@mui/icons-material'
+import { CallMade, CallReceived, PhoneMissed } from '@mui/icons-material'
+import dayjs from 'dayjs'
+
+const parseSecondsToMinutes = (seconds) => {
+  const minutes = Math.floor(seconds / 60)
+  const remainingSeconds = seconds % 60
+  return `${minutes}m ${remainingSeconds}s`
+}
 
 const Activities = () => {
   const { activities, isLoading } = useActivityContext()
-  console.log(activities)
+  const [selectedActivity, setSelectedActivity] = useState(null)
 
   const groupedByDate = activities.reduce((acc, activity) => {
-    const date = new Date(activity.created_at).toDateString()
+    const date = dayjs(activity.created_at).format('YYYY-MM-DD')
     if (!acc[date]) {
       acc[date] = []
     }
     acc[date].push(activity)
     return acc
   }, {})
-  console.log(groupedByDate)
 
   if (isLoading) return <LoadingOverlay />
 
+  const isInbound = (activity) => activity.direction === 'inbound'
+
   return (
     <Box>
-      <List>
-        {activities.map((a) => (
-          <ListItem key={a.id} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {a.call_type === 'answered' && <Phone color='success' />}
-                {a.call_type === 'missed' && <PhoneMissed color='error' />}
-              </ListItemIcon>
-              <ListItemText
-                primary={`${
-                  a.call_type === 'missed'
-                    ? 'Missed call'
-                    : `${
-                        a.direction === 'inbound' ? 'Incoming' : 'Outgoing'
-                      }: ${a.duration} sec`
-                }`}
-                secondary={a.direction === 'inbound' ? a.from : a.to}
-              />
-              <ListItemSecondaryAction>
-                {a.direction === 'inbound' ? <CallReceived /> : <CallMade />}
-              </ListItemSecondaryAction>
-            </ListItemButton>
-          </ListItem>
+      <Box>
+        <Button size='small'>Archive All</Button>
+      </Box>
+      {Object.keys(groupedByDate)
+        .sort((a, b) => dayjs(b).unix() - dayjs(a).unix())
+        .map((date) => (
+          <List
+            key={date}
+            dense
+            subheader={
+              <Box sx={{ my: 1, textAlign: 'center' }}>
+                <Chip label={dayjs(date).format('DD MMMM YYYY')} />
+              </Box>
+            }
+          >
+            {groupedByDate[date]
+              .sort(
+                (a, b) =>
+                  dayjs(b.created_at).unix() - dayjs(a.created_at).unix()
+              )
+              .map((a, index) => (
+                <ListItem
+                  key={a.id}
+                  disablePadding
+                  dense
+                  divider={index === groupedByDate[date].length - 1}
+                >
+                  <ListItemButton onClick={() => setSelectedActivity(a)}>
+                    <ListItemText
+                      primary={dayjs(a.created_at).format('hh:mma')}
+                      sx={{ flex: '0 1 auto', pr: 3 }}
+                    />
+                    <ListItemText
+                      primary={
+                        <>
+                          {a.call_type === 'missed'
+                            ? 'Missed call'
+                            : `${isInbound(a) ? 'Incoming' : 'Outgoing'}: ${
+                                a.duration > 60
+                                  ? parseSecondsToMinutes(a.duration)
+                                  : `${a.duration}s`
+                              }`}
+                        </>
+                      }
+                      secondary={isInbound(a) ? a.from : a.to}
+                    />
+                    <ListItemSecondaryAction>
+                      {a.call_type === 'missed' ? (
+                        <PhoneMissed color='error' />
+                      ) : (
+                        <>{isInbound(a) ? <CallReceived /> : <CallMade />}</>
+                      )}
+                    </ListItemSecondaryAction>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+          </List>
         ))}
-      </List>
     </Box>
   )
 }
